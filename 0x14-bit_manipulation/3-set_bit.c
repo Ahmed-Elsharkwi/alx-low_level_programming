0337f83ed109ad2eeffffe1c3d641d7c9ed32360
ahmed <ahmedelsharkwiti@gmail.com>
2023-08-04 09:07:11 -0700

    MM

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -39,7 +39,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	{
 		sum = sum + (1 * pow_1(2, index));
 	}
-	if (index > *n)
+	if (index == 2000)
 		return (-1);
 	*n = sum;
 	if (n != NULL)
