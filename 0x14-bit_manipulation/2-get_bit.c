ae39174def95121d7d503f54d590f90688d1611d
Ahmed <ahmedelsharkwiti@gmail.com>
2023-08-31 18:04:46 +0300

    MM

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"main.h"
+int len(int u);
+/**
+ * get_bit - get a bit in a specific indix
+ * @n: is an int
+ * @index: is an int
+ * Return: 0 (success)
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int *p = malloc(sizeof(int) * len(n));
+	unsigned int i, l = len(n);
+	unsigned int j;
+
+	if (p == NULL)
+		return (-1);
+	for (i = 0 ; i < l ; i++)
+	{
+		p[i] = n % 2;
+		n = n / 2;
+	}
+	for (j = 0 ; j < l ; j++)
+	{
+		if (j == index)
+			return (p[j]);
+	}
+	if (j < index)
+		return (0);
+	return (-1);
+}
+/**
+ * len - calculate the len of any number
+ * @u: is an int
+ * Return: 0 (success)
+ */
+int len(int u)
+{
+	unsigned int count = 0;
+
+	while (u != 0)
+	{
+		u = u >> 1;
+		count++;
+	}
+	return (count);
+}
