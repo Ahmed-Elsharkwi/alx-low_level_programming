7a15518f0e8954fd502d6cb7849621ca1e8a32fd
Ahmed <ahmedelsharkwiti@gmail.com>
2023-10-13 00:41:31 +0300

    MM

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	if (idx == 0)
+	if (idx == 0 && *h != NULL)
 	{
 		new->next = temp;
 		temp->prev = new;
