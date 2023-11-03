e8bf1a1ae49801337ba91160eaeb798ea935b6cf
Ahmed <ahmedelsharkwiti@gmail.com>
2023-11-03 23:47:50 +0200

    MM

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	int index;
 	hash_node_t *new_node = NULL;
 
-	if (key == NULL)
+	if (ht == NULL || key == NULL)
 		return (0);
 	index = key_index((unsigned char *) key, ht->size);
 	if ((ht->array[index] != NULL) && (strcmp(ht->array[index]->key, key) == 0))
