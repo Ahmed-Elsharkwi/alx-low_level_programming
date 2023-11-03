c58227ad4a141a51a668d5c95e5a4a7a0f233f0f
Ahmed <ahmedelsharkwiti@gmail.com>
2023-11-03 21:08:38 +0200

    MM

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,55 @@
+#include "hash_tables.h"
+hash_node_t *add_node(const char *key, const char *value);
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: is a pointer to hash table
+ * @key: is a string
+ * @value: is a string
+ * Return: 1 if the function success 0 if not
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int index;
+	hash_node_t *new_node = NULL;
+
+	if (key == NULL)
+		return (0);
+	index = key_index((unsigned char *) key, ht->size);
+	if ((ht->array[index] != NULL) && (strcmp(ht->array[index]->key, key) == 0))
+	{
+		strcpy(ht->array[index]->value, value);
+		return (1);
+	}
+	new_node = add_node(key, value);
+	if (new_node == NULL)
+		return (0);
+	if (ht->array[index] == NULL)
+		new_node->next = NULL;
+	else if (ht->array[index] != NULL)
+		new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
+/**
+ * add_node - add a new node
+ * @key: is a string
+ * @value: is a string
+ * Return: the address of the new node
+ */
+hash_node_t *add_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t *));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->key = malloc(strlen(key) + 1);
+	if (new_node->key == NULL)
+		return (NULL);
+	new_node->value = malloc(strlen(key) + 1);
+	if (new_node->value == NULL)
+		return (NULL);
+	strcpy(new_node->key, key);
+	new_node->value = strdup(value);
+	return (new_node);
+}
