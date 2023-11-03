b345ea8783b7bd2e6f88e71f655ab88c9dd88510
Ahmed <ahmedelsharkwiti@gmail.com>
2023-11-03 18:27:15 +0200

    MM

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,19 +1,23 @@
 #include "hash_tables.h"
+
+
 /**
- * hash_table_create - creat a hash table
- * @size: is a long int and size of the array
- * Return: a pointer to the new hash table
+ * hash_table_create - function that creates a hash table
+ * @size: size of the table
+ * Return: a hash_table_t pointer to the created table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 
-	table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
+
 	table->size = size;
-	table->array = malloc(size * sizeof(hash_node_t *));
+	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 		return (NULL);
+
 	return (table);
 }
---
+++ 0x1A-hash_tables/0-hashs_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creat a hash table
+ * @size: is a long int and size of the array
+ * Return: a pointer to the new hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = malloc(size * sizeof(hash_node_t *));
+	if (table->array == NULL)
+		return (NULL);
+	return (table);
+}
