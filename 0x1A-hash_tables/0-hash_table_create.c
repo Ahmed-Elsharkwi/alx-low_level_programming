8c5a17fc8f84512d70049c4b94301346a3217a01
Ahmed <ahmedelsharkwiti@gmail.com>
2023-11-03 18:17:20 +0200

    MM

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,8 +7,11 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	hash_table_t *table;
 
+	if (size == 0)
+		return (NULL);
+	table = (hash_table_t *) malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
