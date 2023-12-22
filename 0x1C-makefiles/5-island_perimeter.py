8e1c411c74666ee4003e76a16dbd3c9000e8f872
Ahmed <ahmedelsharkwiti@gmail.com>
2023-12-22 21:26:55 +0200

    MM

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,13 +5,28 @@
 def island_perimeter(grid):
     """
     calculate the perimeter of the island by iterating over
-    the grid and each time it will find 1 which repersents the land
-    it increase the number of lands 1 and after that we will calculate
-    the perimeter of the land using this equation (number_lands + 1) * 2
+    the grid and when it will find number 1 in the cell it
+    will check if there is any number 1 around it and if there is
+    it will subtracket the number_lands by 1
     """
     number_lands = 0
-    for list in grid:
-        for cell in list:
-            if cell == 1:
-                number_lands += 1
-    return (number_lands + 1) * 2
+    rows = len(grid)
+    columns = len(grid[0])
+
+    for row in range(rows):
+        for column in range(columns):
+            if grid[row][column] == 1:
+                number_lands += 4
+                if (row + 1 != rows) and (
+                        grid[row + 1][column] == 1):
+                    number_lands -= 1
+
+                if (row != 0) and (grid[row - 1][column] == 1):
+                    number_lands -= 1
+
+                if (column + 1 != columns) and (grid[row][column + 1] == 1):
+                    number_lands -= 1
+
+                if (column != 0) and (grid[row][column - 1] == 1):
+                    number_lands -= 1
+    return number_lands
