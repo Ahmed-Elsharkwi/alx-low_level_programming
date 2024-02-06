2630382c8f3e6f436a8cc75f81864a3cf4d53a18
Ahmed <ahmedelsharkwiti@gmail.com>
2024-02-06 16:48:11 +0200

    commit the first search algrothime

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+/*
+ * linear_search - get the index of the wanted value
+ * @array: array of intgers
+ * @size: the size of the array
+ * @value: the wanted value
+ * Return: return the index of the wanted value or -1 in case of failure
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	while (i < size)
+	{
+		if (value == array[i])
+			return (i);
+
+		else
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		}
+		i++;
+	}
+	return (-1);
+}
